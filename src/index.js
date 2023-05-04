65ee46d47e7a7897f6c0b450bf39c10173f5d13a
Hennadii Bublikov <99074724+HennadiiBu@users.noreply.github.com>
2023-05-05 00:11:28 +0300

    it`s work

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import { refs } from './partials/refs';
 import { fetchPixabay } from './partials/pixabayAPI';
 import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 refs.searchForm.addEventListener('submit', onUserSearchSub);
 refs.loadMoreBtn.addEventListener('click', onClickLoadMore);
@@ -10,14 +12,23 @@ let page = 1;
 async function onClickLoadMore() {
   page += 1;
 
-const userSearchValue = localStorage.getItem('userSearch')
+  if (page === 13) {
+    Notiflix.Notify.info(
+      "We're sorry, but you've reached the end of search results."
+    );
+    refs.loadMoreBtn.classList.toggle('hidden')
+  }
+
+  const userSearchValue = localStorage.getItem('userSearch');
 
   const { hits } = await fetchPixabay(userSearchValue, page);
 
   const marckup = hits.map(
     elem => `
   <div class="photo-card">
+  <a href="${elem.largeImageURL}">
     <img class="photo-card__image" src="${elem.webformatURL}" alt="${elem.tags}" width="180px" height="180px" loading="lazy" />
+  </a>
     <div class="info">
         <p class="info-item">
             <b>Likes</b>
@@ -51,12 +62,14 @@ async function onUserSearchSub(event) {
 
   refs.imageContainer.innerHTML = '';
 
-  const { hits } = await fetchPixabay(searchQuery.value, page);
+  const { hits, totalHits } = await fetchPixabay(searchQuery.value, page);
 
   const marckup = hits.map(
     elem => `
   <div class="photo-card">
-    <img class="photo-card__image" src="${elem.webformatURL}" alt="${elem.tags}" width="180px" height="180px" loading="lazy" />
+    <a href="${elem.largeImageURL}">
+      <img class="photo-card__image" src="${elem.webformatURL}" alt="${elem.tags}" width="180px" height="180px" loading="lazy" />
+    </a>
     <div class="info">
         <p class="info-item">
             <b>Likes</b>
@@ -78,5 +91,14 @@ async function onUserSearchSub(event) {
   </div>`
   );
 
+  Notiflix.Notify.info(`Hooray! We found ${totalHits} images.`);
+
   refs.imageContainer.insertAdjacentHTML('beforeend', marckup.join());
+  refs.loadMoreBtn.classList.toggle('hidden')
+
+
 }
+
+var lightbox = new SimpleLightbox('.photo-card a');
+
+
--- src/partials/pixabayAPI.js
+++ src/partials/pixabayAPI.js
@@ -12,7 +12,7 @@ import Notiflix from 'notiflix';
 
 async function fetchPixabay(searchQuery, pageNum=1) {
   try {
-    const responce = await fetch(
+    const responce = await fetch( 
       `https://pixabay.com/api/?key=35917773-69c30edf6ec6a269aa0ed0b0d&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${pageNum}`
     );
     return await responce.json();
